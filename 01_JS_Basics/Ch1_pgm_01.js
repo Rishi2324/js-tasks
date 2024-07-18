5b47b09f3e73c64db170276e381c8f52381f708f
Rishi2324 <144668202+Rishi2324@users.noreply.github.com>
2024-07-18 10:23:10 +0530

    Add files via upload

---
+++ 01_JS_Basics/Ch1_pgm_01.js
@@ -0,0 +1,20 @@
+// Using console.log to display information
+
+console.log("Good morning!");
+
+// Adding another console.log statement
+console.log("This is another message.");
+
+/* Further Adventures
+ *
+ * 1) Change the text between the parentheses.
+ *
+ * 2) Click Run on the console to run the program again.
+ *
+ * 3) Add your own console.log statement
+ *    to the program, so there are two.
+ *    Don't forget the semicolon.
+ *
+ * 4) Click Run.
+ *
+ */
