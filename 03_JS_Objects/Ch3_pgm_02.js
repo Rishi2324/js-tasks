ad8f4f661f6d4a252234866d3878c24439cb8638
Rishi2324 <144668202+Rishi2324@users.noreply.github.com>
2024-07-28 14:27:01 +0300

    Add files via upload

---
+++ 03_JS_Objects/Ch3_pgm_02.js
@@ -0,0 +1,47 @@
+// Using prefixes to tell book variables apart
+
+var book1Title = "The Hobbit";
+var book1Author = "J.R.R.Tolkien";
+
+var book2Title = "Northern Lights";
+var book2Author = "Philip Pullman";
+
+var book3Title = "The Adventures of Tom Sawyer";
+var book3Author = "Mark Twain";
+
+console.log("There are three books so far...");
+console.log(book1Title + " by " + book1Author);
+console.log(book2Title + " by " + book2Author);
+console.log(book3Title + " by " + book3Author);
+
+
+
+/* Further Adventures
+ *
+ * 1) Add a fourth book.
+ *
+ * 2) Update the messages to include the new book.
+ *
+ */
+// Variables for the first book
+var book1Title = "The Hobbit";
+var book1Author = "J.R.R.Tolkien";
+
+// Variables for the second book
+var book2Title = "Northern Lights";
+var book2Author = "Philip Pullman";
+
+// Variables for the third book
+var book3Title = "The Adventures of Tom Sawyer";
+var book3Author = "Mark Twain";
+
+// Variables for the fourth book
+var book4Title = "Pride and Prejudice";
+var book4Author = "Jane Austen";
+
+// Logging details for the books
+console.log("There are four books so far...");
+console.log(book1Title + " by " + book1Author);
+console.log(book2Title + " by " + book2Author);
+console.log(book3Title + " by " + book3Author);
+console.log(book4Title + " by " + book4Author);
