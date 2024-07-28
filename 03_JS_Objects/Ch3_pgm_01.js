3dc588935fd47eb1a66746b5232df828b120addb
Rishi2324 <144668202+Rishi2324@users.noreply.github.com>
2024-07-28 14:24:39 +0300

    Add files via upload

---
+++ 03_JS_Objects/Ch3_pgm_01.js
@@ -0,0 +1,84 @@
+
+var bookTitle1 = "The Hobbit";
+var bookAuthor1 = "J. R. R. Tolkien";
+
+// Variables for the second book
+var bookTitle2 = "1984";
+var bookAuthor2 = "George Orwell";
+
+// Variables for the third book
+var bookTitle3 = "To Kill a Mockingbird";
+var bookAuthor3 = "Harper Lee";
+
+// Logging details for the first book
+console.log(bookTitle1 + " by " + bookAuthor1);
+
+// Logging details for the second book
+console.log(bookTitle2 + " by " + bookAuthor2);
+
+// Logging details for the third book
+console.log(bookTitle3 + " by " + bookAuthor3);
+
+// Managing 10 books using arrays
+var bookTitles10 = [
+  "The Hobbit",
+  "1984",
+  "To Kill a Mockingbird",
+  "The Great Gatsby",
+  "Moby Dick",
+  "Pride and Prejudice",
+  "War and Peace",
+  "The Catcher in the Rye",
+  "Jane Eyre",
+  "Brave New World"
+];
+
+var bookAuthors10 = [
+  "J. R. R. Tolkien",
+  "George Orwell",
+  "Harper Lee",
+  "F. Scott Fitzgerald",
+  "Herman Melville",
+  "Jane Austen",
+  "Leo Tolstoy",
+  "J.D. Salinger",
+  "Charlotte Brontë",
+  "Aldous Huxley"
+];
+
+// Logging details for 10 books
+for (var i = 0; i < bookTitles10.length; i++) {
+  console.log(bookTitles10[i] + " by " + bookAuthors10[i]);
+}
+
+// Managing 100 books using arrays
+var bookTitles100 = [
+  "Book 1", "Book 2", "Book 3", "Book 4", "Book 5", "Book 6", "Book 7", "Book 8", "Book 9", "Book 10",
+  "Book 11", "Book 12", "Book 13", "Book 14", "Book 15", "Book 16", "Book 17", "Book 18", "Book 19", "Book 20",
+  "Book 21", "Book 22", "Book 23", "Book 24", "Book 25", "Book 26", "Book 27", "Book 28", "Book 29", "Book 30",
+  "Book 31", "Book 32", "Book 33", "Book 34", "Book 35", "Book 36", "Book 37", "Book 38", "Book 39", "Book 40",
+  "Book 41", "Book 42", "Book 43", "Book 44", "Book 45", "Book 46", "Book 47", "Book 48", "Book 49", "Book 50",
+  "Book 51", "Book 52", "Book 53", "Book 54", "Book 55", "Book 56", "Book 57", "Book 58", "Book 59", "Book 60",
+  "Book 61", "Book 62", "Book 63", "Book 64", "Book 65", "Book 66", "Book 67", "Book 68", "Book 69", "Book 70",
+  "Book 71", "Book 72", "Book 73", "Book 74", "Book 75", "Book 76", "Book 77", "Book 78", "Book 79", "Book 80",
+  "Book 81", "Book 82", "Book 83", "Book 84", "Book 85", "Book 86", "Book 87", "Book 88", "Book 89", "Book 90",
+  "Book 91", "Book 92", "Book 93", "Book 94", "Book 95", "Book 96", "Book 97", "Book 98", "Book 99", "Book 100"
+];
+
+var bookAuthors100 = [
+  "Author 1", "Author 2", "Author 3", "Author 4", "Author 5", "Author 6", "Author 7", "Author 8", "Author 9", "Author 10",
+  "Author 11", "Author 12", "Author 13", "Author 14", "Author 15", "Author 16", "Author 17", "Author 18", "Author 19", "Author 20",
+  "Author 21", "Author 22", "Author 23", "Author 24", "Author 25", "Author 26", "Author 27", "Author 28", "Author 29", "Author 30",
+  "Author 31", "Author 32", "Author 33", "Author 34", "Author 35", "Author 36", "Author 37", "Author 38", "Author 39", "Author 40",
+  "Author 41", "Author 42", "Author 43", "Author 44", "Author 45", "Author 46", "Author 47", "Author 48", "Author 49", "Author 50",
+  "Author 51", "Author 52", "Author 53", "Author 54", "Author 55", "Author 56", "Author 57", "Author 58", "Author 59", "Author 60",
+  "Author 61", "Author 62", "Author 63", "Author 64", "Author 65", "Author 66", "Author 67", "Author 68", "Author 69", "Author 70",
+  "Author 71", "Author 72", "Author 73", "Author 74", "Author 75", "Author 76", "Author 77", "Author 78", "Author 79", "Author 80",
+  "Author 81", "Author 82", "Author 83", "Author 84", "Author 85", "Author 86", "Author 87", "Author 88", "Author 89", "Author 90",
+  "Author 91", "Author 92", "Author 93", "Author 94", "Author 95", "Author 96", "Author 97", "Author 98", "Author 99", "Author 100"
+];
+
+// Logging details for 100 books
+for (var i = 0; i < bookTitles100.length; i++) {
+  console.log(bookTitles100[i] + " by " + bookAuthors100[i]);
+}
